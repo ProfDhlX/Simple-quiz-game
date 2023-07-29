2e070f99733d3ed2b54d9799c842c98ad2aa81ef
Kewal <110725776+ProfDhlX@users.noreply.github.com>
2023-07-29 18:09:24 +0545

    Update data.js
    page doesn't showing up the questions

--- data.js
+++ data.js
@@ -1,81 +1,102 @@
 const quizData = [
-  {Question: "What is the capital city of France?",
-Options: ["Paris", "Marseille", "Lyon", "Nice"],
-Answer: "Paris"},
-
-{Question: "What is the largest desert in the world?",
-Options: ["Sahara", "Gobi", "Arabian", "Kalahari"],
-Answer: "Sahara"},
-
-{Question: "Which planet is known as the 'Red Planet'?",
-Options: ["Mars", "Venus", "Mercury", "Jupiter"],
-Answer: "Mars"},
-
-{Question: "What is the currency of Japan?",
-Options: ["Yen", "Won", "Rupee", "Euro"],
-Answer: "Yen"},
-
-{Question: "Which famous scientist formulated the theory of relativity?",
-Options: ["Albert Einstein", "Isaac Newton", "Galileo Galilei", "Marie Curie"],
-Answer: "Albert Einstein"},
-
-{Question: "What is the tallest mountain in the world?",
-Options: ["Mount Everest", "K2", "Kangchenjunga", "Makalu"],
-Answer: "Mount Everest"},
-
-{Question: "Which country is known as the 'Land of the Rising Sun'?",
-Options: ["Japan", "China", "Korea", "Thailand"],
-Answer: "Japan"},
-
-{Question: "Who wrote the play 'Romeo and Juliet'?",
-Options: ["William Shakespeare", "Jane Austen", "Charles Dickens", "Leo Tolstoy"],
-Answer: "William Shakespeare"},
-
-{Question: "What is the largest ocean on Earth?",
-Options: ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean"],
-Answer: "Pacific Ocean"},
-
-{Question: "Which famous painting is also known as 'La Gioconda'?",
-Options: ["Mona Lisa", "The Starry Night", "The Last Supper", "The Scream"],
-Answer: "Mona Lisa"},
-
-{Question: "What is the national flower of India?",
-Options: ["Lotus", "Rose", "Tulip", "Jasmine"],
-Answer: "Lotus"},
-
-{Question: "Who is the author of the 'Harry Potter' book series?",
-Options: ["J.K. Rowling", "J.R.R. Tolkien", "George R.R. Martin", "Stephen King"],
-Answer: "J.K. Rowling"},
-
-{Question: "Which gas do plants absorb from the atmosphere during photosynthesis?",
-Options: ["Carbon dioxide", "Oxygen", "Nitrogen", "Hydrogen"],
-Answer: "Carbon dioxide"},
-
-{Question: "What is the largest mammal in the world?",
-Options: ["Blue whale", "African elephant", "Giraffe", "Polar bear"],
-Answer: "Blue whale"},
-
-{Question: "Which instrument is known as the 'King of Instruments'?",
-Options: ["Pipe organ", "Piano", "Violin", "Flute"],
-Answer: "Pipe organ"},
-
-{Question: "What is the currency of the United Kingdom?",
-Options: ["Pound sterling", "Euro", "Dollar", "Yen"],
-Answer: "Pound sterling"},
-
-{Question: "Who painted the famous artwork 'The Starry Night'?",
-Options: ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Claude Monet"],
-Answer: "Vincent van Gogh"},
-
-{Question: "What is the largest continent in the world?",
-Options: ["Asia", "Africa", "North America", "South America"],
-Answer: "Asia"},
-
-{Question: "Which planet is known as the 'Morning Star' or 'Evening Star'?",
-Options: ["Venus", "Mars", "Jupiter", "Saturn"],
-Answer: "Venus"},
-  
-{Question: "What is the capital city of Brazil?",
-Options: ["Brasília", "Rio de Janeiro", "São Paulo", "Salvador"],
-Answer: "Brasília"},
+    {
+        Question: "What is the capital city of France?",
+        Options: ["Paris", "Marseille", "Lyon", "Nice"],
+        Answer: "Paris"
+    },
+    {
+        Question: "What is the largest desert in the world?",
+        Options: ["Sahara", "Gobi", "Arabian", "Kalahari"],
+        Answer: "Sahara"
+    },
+    {
+        Question: "Which planet is known as the 'Red Planet'?",
+        Options: ["Mars", "Venus", "Mercury", "Jupiter"],
+        Answer: "Mars"
+    },
+    {
+        Question: "What is the currency of Japan?",
+        Options: ["Yen", "Won", "Rupee", "Euro"],
+        Answer: "Yen"
+    },
+    {
+        Question: "Which famous scientist formulated the theory of relativity?",
+        Options: ["Albert Einstein", "Isaac Newton", "Galileo Galilei", "Marie Curie"],
+        Answer: "Albert Einstein"
+    },
+    {
+        Question: "What is the tallest mountain in the world?",
+        Options: ["Mount Everest", "K2", "Kangchenjunga", "Makalu"],
+        Answer: "Mount Everest"
+    },
+    {
+        Question: "Which country is known as the 'Land of the Rising Sun'?",
+        Options: ["Japan", "China", "Korea", "Thailand"],
+        Answer: "Japan"
+    },
+    {
+        Question: "Who wrote the play 'Romeo and Juliet'?",
+        Options: ["William Shakespeare", "Jane Austen", "Charles Dickens", "Leo Tolstoy"],
+        Answer: "William Shakespeare"
+    },
+    {
+        Question: "What is the largest ocean on Earth?",
+        Options: ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean"],
+        Answer: "Pacific Ocean"
+    },
+    {
+        Question: "Which famous painting is also known as 'La Gioconda'?",
+        Options: ["Mona Lisa", "The Starry Night", "The Last Supper", "The Scream"],
+        Answer: "Mona Lisa"
+    },
+    {
+        Question: "What is the national flower of India?",
+        Options: ["Lotus", "Rose", "Tulip", "Jasmine"],
+        Answer: "Lotus"
+    },
+    {
+        Question: "Who is the author of the 'Harry Potter' book series?",
+        Options: ["J.K. Rowling", "J.R.R. Tolkien", "George R.R. Martin", "Stephen King"],
+        Answer: "J.K. Rowling"
+    },
+    {
+        Question: "Which gas do plants absorb from the atmosphere during photosynthesis?",
+        Options: ["Carbon dioxide", "Oxygen", "Nitrogen", "Hydrogen"],
+        Answer: "Carbon dioxide"
+    },
+    {
+        Question: "What is the largest mammal in the world?",
+        Options: ["Blue whale", "African elephant", "Giraffe", "Polar bear"],
+        Answer: "Blue whale"
+    },
+    {
+        Question: "Which instrument is known as the 'King of Instruments'?",
+        Options: ["Pipe organ", "Piano", "Violin", "Flute"],
+        Answer: "Pipe organ"
+    },
+    {
+        Question: "What is the currency of the United Kingdom?",
+        Options: ["Pound sterling", "Euro", "Dollar", "Yen"],
+        Answer: "Pound sterling"
+    },
+    {
+        Question: "Who painted the famous artwork 'The Starry Night'?",
+        Options: ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Claude Monet"],
+        Answer: "Vincent van Gogh"
+    },
+    {
+        Question: "What is the largest continent in the world?",
+        Options: ["Asia", "Africa", "North America", "South America"],
+        Answer: "Asia"
+    },
+    {
+        Question: "Which planet is known as the 'Morning Star' or 'Evening Star'?",
+        Options: ["Venus", "Mars", "Jupiter", "Saturn"],
+        Answer: "Venus"
+    },
+    {
+        Question: "What is the capital city of Brazil?",
+        Options: ["Brasília", "Rio de Janeiro", "São Paulo", "Salvador"],
+        Answer: "Brasília"
+    }
 ];
